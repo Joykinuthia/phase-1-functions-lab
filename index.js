9d8bcd1751c79b970a981d9387b809d8cfe78e50
Joykinuthia <joyrosekinuthia@gmail.com>
2025-03-05 18:06:52 +0300

    functions

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+const hqStreet = 42;
+return Math.abs(street-hqStreet);
+}
+
+function distanceFromHqInBlocks(street) {
+    const hqStreet = 42;
+    return Math.abs(street-hqStreet);
+}
+
+function distanceFromHqInFeet(street) {
+    const hqStreet = 42;
+    return distanceFromHqInBlocks(street) * 264;
+    distanceFromHqInBlocks()
+}
+
+function distanceFromHqInFeet(street){
+    const hqStreet = 42;
+    return distanceFromHqInBlocks(street) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const hqStreet = 42;
+    return Math.abs(start-destination) * 264;
+}
+
+function distanceTravelledInFeet( start, destination) {
+    const hqStreet = 42;
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start,destination);
+    if (distance <= 400) {
+        return 0;
+    }else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02;
+    }else if  (distance >2000 && distance <= 2500){
+        return 25;
+    }else {
+        return "cannot travel that far";
+    }
+}
